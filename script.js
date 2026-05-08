6176e067fbc85a6a94008f508302cdf5a7f88fde
yusuke1976 <168104566+yusuke1976@users.noreply.github.com>
2026-05-08 17:16:33 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// AzureのURLではなく、自分のAPIルートを叩く
-const response = await fetch("/api/GetAIAnswer", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ message: messageText })
-});
-const data = await response.json();
-addMessage(data.answer, 'bot');
